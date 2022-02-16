b9e07bb68d7f6c7625820c769ff9b00fb274dfae
MUHSHIN KHAN <usa718@gmail.com>
2022-02-15 22:17:51 -0500

    Test

---
+++ mvnFW/src/test/java/proj001/firstTestNG.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package proj001;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author usa71
+ *
+ */
+public class firstTestNG {
+	
+	@Test
+	public void PrintMessage( ) {
+		System.out.println("Hello World!");
+		System.out.println("This is line 2");
+	}
+
+}
