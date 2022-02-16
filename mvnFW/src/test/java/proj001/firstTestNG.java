d55d01894e39fb780ec590840bfa18de9096ff76
MUHSHIN KHAN <usa718@gmail.com>
2022-02-15 22:18:45 -0500

    This is test.

--- mvnFW/src/test/java/proj001/firstTestNG.java
+++ mvnFW/src/test/java/proj001/firstTestNG.java
@@ -14,7 +14,8 @@ public class firstTestNG {
 	@Test
 	public void PrintMessage( ) {
 		System.out.println("Hello World!");
-		System.out.println("This is line 2");
+		System.out.println("This is line 2"); 
+		System.out.println("This is line 3");
 	}
 
 }
